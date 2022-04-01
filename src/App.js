fe4d2c93b132ae366920422611f9cfd300753076
0x1byte <mahdikh6@gmail.com>
2022-04-01 13:27:15 +0430

    first shop

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes,Route,Navigate } from "react-router-dom";
+
+// Context
+import ProductContextProvider from "./context/ProductContextProvider";
+import CartContextProvider from "./context/CartContextProvider";
+// Components
+import Store from './components/Store';
+import ProductDetails from "./components/ProductDetails";
+import NavBar from "./components/shared/NavBar";
+import ShopCart from "./components/ShopCart";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ProductContextProvider>
+      <CartContextProvider>
+        <NavBar />
+        <Routes>
+          <Route path="/products/:id" element={<ProductDetails />} />
+          <Route path="/products" element={<Store />} />
+          <Route path="/cart" element={<ShopCart />} />
+          <Route path="/*" element={<Navigate to="/products" />} />
+        </Routes>
+      </CartContextProvider>
+    </ProductContextProvider>
   );
 }
 
---
+++ src/components/shared/NavBar.js
@@ -0,0 +1,31 @@
+import React , { useContext } from 'react';
+import { Link } from 'react-router-dom';
+
+// Context
+import { CartContext } from '../../context/CartContextProvider';
+
+// Style
+import styles from "./NavBar.module.css";
+
+// Icons
+import shopIcon from "../../assets/icons/shop.svg";
+
+
+const NavBar = () => {
+
+    const {state} = useContext(CartContext);
+
+  return (
+    <div  className={styles.mainContainer}>
+      <div className={styles.container}>
+        <Link className={styles.productLink} to="/products">Products</Link>
+        <div className={styles.iconContainer}>
+            <Link to="/cart"><img src={shopIcon} alt="" /></Link>
+            <span>{state.itemsCounter}</span>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default NavBar;
